a07f5c6ca2969fa5d770fe6c454b1093d4e7d881
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2020-12-03 21:16:27 +0000

    add an extra endpoint

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = (app) => {
+
+  // GET /api/user (CRUD -> READ)
   app.get(`/api/user`, async (req, res) => {
     const persons = await User.find();
     return res.status(200).send(persons);
   });
 
+  // POST /api/user (CRUD -> CREATION)
   app.post(`/api/user`, async (req, res) => {
     const user = await User.create(req.body);
     return res.status(201).send({
@@ -15,6 +18,20 @@ module.exports = (app) => {
     });
   });
 
+  // GET /api/user/:id (CRUD -> READ)
+  app.get(`/api/user/:id`, async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    return res.status(200).send({
+      error: false,
+      user,
+    });
+  });
+
+  // PUT /api/user/:id (CRUD -> UPDATE)
+  // PUT /api/user/5fc94978f648a175792c30d3
   app.put(`/api/user/:id`, async (req, res) => {
     const { id } = req.params;
 
@@ -26,6 +43,7 @@ module.exports = (app) => {
     });
   });
 
+  // DELETE /api/user/:id (CRUD -> DELETE)
   app.delete(`/api/user/:id`, async (req, res) => {
     const { id } = req.params;
 
